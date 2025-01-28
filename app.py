c3163f714de23165c00bb711f8734f14f498e368
dapetri <me@dapetri.com>
2025-01-28 21:09:40 +0000

    fixing postgres auth

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from src.models import Location, db
 logger.add("logs/app.log", rotation="500 MB", level="INFO")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
